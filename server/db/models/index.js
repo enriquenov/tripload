42cc094ca14a9b61cc9aa44084812ee86f04f8f6
enrique novoa <luisenriquenovoa@gmail.com>
2019-03-03 17:48:39 -0800

    Feat: Created associations between users, trips and adventures models.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,19 +2,14 @@ const User = require('./user')
 const Trip = require('./trip')
 const Adventure = require('./adventure')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+// Model's Associations
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+User.hasMany(Trip)
+Trip.belongsTo(User)
+
+Trip.hasMany(Adventure)
+Adventure.belongsTo(Trip)
+
 module.exports = {
   User,
   Trip,
