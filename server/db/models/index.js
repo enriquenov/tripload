a4897d5a37e6f22393ed776c357ceb229475aa77
Enrique Novoa <luisenriquenovoa@gmail.com>
2019-03-03 22:17:53 -0800

    Creating transition model (#13)
    * Feat: Created transition model.
    
    * Feat: Added associations between transition and trip models.
    
    * Feat: Seeded dummy transitions.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./user')
 const Trip = require('./trip')
 const Adventure = require('./adventure')
+const Transition = require('./transition')
 
 // Model's Associations
 
@@ -8,10 +9,13 @@ User.hasMany(Trip)
 Trip.belongsTo(User)
 
 Trip.hasMany(Adventure)
+Trip.hasMany(Transition)
 Adventure.belongsTo(Trip)
+Transition.belongsTo(Trip)
 
 module.exports = {
   User,
   Trip,
-  Adventure
+  Adventure,
+  Transition
 }
---
+++ server/db/models/transition.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Transition = db.define('transition', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: 'Cross-adventure'
+  },
+  type: {
+    type: Sequelize.ENUM('Ground', 'Air', 'Ocean'),
+    allowNull: true
+  }
+})
+
+module.exports = Transition
