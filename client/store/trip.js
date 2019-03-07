7213fff218f1950c1ac45207f6fb3b91f851e2e4
enrique novoa <luisenriquenovoa@gmail.com>
2019-03-06 22:13:57 -0800

    Feat: Trip reducer created.

--- client/store/trip.js
+++ client/store/trip.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
-import history from '../history'
 
 /**
  * ACTION TYPES
  */
 const GET_TRIPS = 'GET_TRIPS'
-// const REMOVE_USER = 'REMOVE_USER'
 
 /**
  * INITIAL STATE
@@ -16,45 +14,26 @@ const defaultTrips = {}
  * ACTION CREATORS
  */
 const getTrips = trips => ({type: GET_TRIPS, trips})
-// const removeUser = () => ({type: REMOVE_USER})
 
 /**
  * THUNK CREATORS
  */
-export const gotTrips = () => async dispatch => {
+export const gotTrips = userId => async dispatch => {
   try {
-    const res = await axios.get('/api/')
-    dispatch(getUser(res.data || defaultUser))
+    const res = await axios.get(`/api/users/${userId}/yourtrips`)
+    dispatch(getTrips(res.data))
   } catch (err) {
     console.error(err)
-  }
-}
-
-export const auth = (email, password, method) => async dispatch => {
-  let res
-  try {
-    res = await axios.post(`/auth/${method}`, {email, password})
-  } catch (authError) {
-    return dispatch(getUser({error: authError}))
-  }
-
-  try {
-    dispatch(getUser(res.data))
-    history.push('/yourtrips')
-  } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
   }
 }
 
 /**
  * REDUCER
  */
-export default function(state = defaultUser, action) {
+export default function(state = defaultTrips, action) {
   switch (action.type) {
-    case GET_USER:
-      return action.user
-    case REMOVE_USER:
-      return defaultUser
+    case GET_TRIPS:
+      return action.trips
     default:
       return state
   }
