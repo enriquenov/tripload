32f17c36ab8c7b9a675c0668dd6fbdff16eb9f46
Enrique Novoa <luisenriquenovoa@gmail.com>
2019-03-07 23:27:32 -0800

    Single trip view (#27)
    * Feat: Added Links to SingleTripCards. Ready to wire it up with the API route.
    
    * Feat: Seeded additional dummy trips. Removed unnecessary logs.
    
    * Feat: Created YourTripsView Component. Ready to implement.

---
+++ client/components/YourTripsView.js
@@ -0,0 +1,33 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+// import {connect} from 'react-redux'
+
+/**
+ * COMPONENT
+ */
+
+export class YourTripsView extends React.Component {
+  render() {
+    return <div className="container yourtrips" />
+  }
+}
+
+/**
+ * CONTAINER
+ */
+// const mapState = state => ({
+//   trips: state.trip
+// })
+
+// const mapDispatch = dispatch => ({
+//   fetchTrips: id => dispatch(gotTrips(id))
+// })
+
+// export default connect(mapState, mapDispatch)(YourTripsView)
+
+/**
+ * PROP TYPES
+ */
+// UserHome.propTypes = {
+//   firstName: PropTypes.string
+// }
