e95902b9e0584a9e62b6e8cf4f9a6213515bac1d
enrique novoa <luisenriquenovoa@gmail.com>
2019-03-19 22:40:22 -0700

    Feat: Wired up the adventures reducer and the singleTripView component with the API route.

--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {default as Home} from './home'
+export {default as SingleTripView} from './SingleTripView'
 export {Login, Signup} from './auth-form'
